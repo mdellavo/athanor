b9d1a56f44413642eb538827fe9825fe469ae721
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2012-04-22 01:39:40 -0400

    fix

--- athanor/__init__.py
+++ athanor/__init__.py
@@ -2,4 +2,4 @@ from athanor.model import Session, BaseDeclarativeMeta, BaseModel, Base
 from athanor.types import UTCDateTime
 from athanor.mixins import StampedMixin, TrackedMixin
 from athanor.utils import underscore, pluralize
-from athanor.decl_enum import DeclEnum
+from athanor.decl_enum import DeclEnum, EnumSymbol
